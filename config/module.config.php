7b98bdf2174ee94f3ae8da267c42a7d7eaecac51
cristian <cristian@192.168.1.140>
2016-02-13 21:02:55 -0300

    Commit

--- config/module.config.php
+++ config/module.config.php
@@ -3,11 +3,26 @@
 return array(
     'cdientity_options' => array(
     ),
+      'doctrine' => array(
+        'driver' => array(
+            // overriding zfc-user-doctrine-orm's config
+            'zfcuser_entity' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'paths' => __DIR__ . '/../src/CdiEntity/Entity',
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    'CdiEntity\Entity' => 'cdientity_entity',
+                ),
+            ),
+        ),
+    ),
      'controllers' => array(
         'invokables' => array(
             'usersession' => 'CdiUser\Controller\UserSessionController',
         ),
     ),
+    
     'router' => array(
         'routes' => array(
             'cdiuser' => array(
