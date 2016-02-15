9823c62b2d6db8cbb44d642f90896901b5fe91f8
cristian <cristian@192.168.1.140>
2016-02-15 00:39:00 -0300

    Commit

---
+++ src/CdiEntity/Service/UpdateEntity.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CdiEntity\Service;
+
+
+/**
+ * TITLE
+ *
+ * Description
+ *
+ * @author Cristian Incarnato <cristian.cdi@gmail.com>
+ *
+ * @package Paquete
+ */
+class updateEntity implements ServiceManagerAwareInterface {
+
+    public function applyChanges(CdiEntity\Entity\Entity $entity) {
+
+        if ($entity->getProperties()->count()) {
+            $newcontent = "//cdientitystart";
+            foreach ($entity->getProperties() as $property) {
+                $newcontent .= "protected " . $property->getName() . ";";
+            }
+            $newcontent .= "\n";
+            $newcontent = "//cdientityend";
+
+
+
+
+            $path_to_file = $entity->getPath();
+            $file_contents = file_get_contents($path_to_file);
+            $file_contents = preg_replace("/\/\/cdientitystart.*\/\/cdientityend/", $newcontent, $file_contents);
+            file_put_contents($path_to_file, $file_contents);
+        }
+    }
+
+}
