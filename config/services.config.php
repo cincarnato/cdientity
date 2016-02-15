9bcffd8233b4b6341f4ea44001410b622d3320ef
cristian <cristian@192.168.12.144>
2016-02-15 16:50:45 -0300

    Commit

--- config/services.config.php
+++ config/services.config.php
@@ -1,20 +1,22 @@
 <?php
 
 /**
- * User: Vladimir Garvardt
- * Date: 3/18/13
- * Time: 6:39 PM
+ * User: Cristian Incarnato
  */
 use Zend\ServiceManager\ServiceLocatorInterface;
 
 return array(
     'invokables' => array(
-         'cdientity_update_entity' => 'CdiEntity\Service\UpdateEntity',
     ),
     'factories' => array(
         'cdientity_options' => function (ServiceLocatorInterface $sm) {
             $config = $sm->get('Config');
-            return new CdiEntity\Options\CdiEntityOptions(isset($config['cdientity_options']) ? $config['cdientity_options'] : array());
+            return new \CdiEntity\Options\CdiEntityOptions(isset($config['cdientity_options']) ? $config['cdientity_options'] : array());
         },
+        'cdientity_update_entity' => function (ServiceLocatorInterface $sm) {
+            $service = new \CdiEntity\Service\UpdateEntity();
+            $service->setServiceManager($sm);
+            return $service;
+        }
+                
     ));
-        
