f09223aa60abd0d593982616deb87d482d5749b5
cristian <cristian@192.168.12.144>
2016-02-15 16:50:42 -0300

    Commit

--- src/CdiEntity/Service/UpdateEntity.php
+++ src/CdiEntity/Service/UpdateEntity.php
@@ -2,7 +2,7 @@
 
 namespace CdiEntity\Service;
 
-//use Zend\ServiceManager\ServiceManagerAwareInterface;
+use Zend\ServiceManager\ServiceManagerAwareInterface;
 
 /**
  * TITLE
@@ -13,13 +13,18 @@ namespace CdiEntity\Service;
  *
  * @package Paquete
  */
-class UpdateEntity {
+class UpdateEntity implements ServiceManagerAwareInterface {
+
+    /**
+     * @var ServiceManager
+     */
+    protected $serviceManager;
 
     public function applyChanges(\CdiEntity\Entity\Entity $entity) {
 
         if ($entity->getProperties()->count()) {
             $newcontent = '//cdientitystart';
-            $newcontent .= PHP_EOL.PHP_EOL;
+            $newcontent .= PHP_EOL . PHP_EOL;
 
 
 
@@ -29,23 +34,23 @@ class UpdateEntity {
                 //Docblocks for anotattions
 
                 $newcontent .= '/**' . PHP_EOL;
-                $newcontent .= '* @Annotation\Options({"label":"'.$property->getName().':"})' . PHP_EOL;
-               
+                $newcontent .= '* @Annotation\Options({"label":"' . $property->getName() . ':"})' . PHP_EOL;
+
                 switch ($property->getType()) {
                     case "string":
-                         $newcontent .= '* @Annotation\Attributes({"type":"text"})' . PHP_EOL;
+                        $newcontent .= '* @Annotation\Attributes({"type":"text"})' . PHP_EOL;
                         $newcontent .= '* @ORM\Column(type="string", length=' . $property->getLength() . ', unique=' . $this->booleanString($property->getBeUnique()) . ', nullable=' . $this->booleanString($property->getBeNullable()) . ', name="' . $property->getTblName() . '")/*' . PHP_EOL;
                         break;
                     case "integer":
-                          $newcontent .= '* @Annotation\Attributes({"type":"text"})' . PHP_EOL;
+                        $newcontent .= '* @Annotation\Attributes({"type":"text"})' . PHP_EOL;
                         $newcontent .= '* @ORM\Column(type="integer", length=' . $property->getLength() . ', unique=' . $this->booleanString($property->getBeUnique()) . ', nullable=' . $this->booleanString($property->getBeNullable()) . ', name="' . $property->getTblName() . '")/*' . PHP_EOL;
                         break;
                     case "text":
-                          $newcontent .= '* @Annotation\Attributes({"type":"textarea"})' . PHP_EOL;
+                        $newcontent .= '* @Annotation\Attributes({"type":"textarea"})' . PHP_EOL;
                         $newcontent .= '* @ORM\Column(type="text", unique=' . $this->booleanString($property->getBeUnique()) . ', nullable=' . $this->booleanString($property->getBeNullable()) . ', name="' . $property->getTblName() . '")/*' . PHP_EOL;
                         break;
                     case "boolean":
-                          $newcontent .= '* @Annotation\Attributes({"type":"checkbox"})' . PHP_EOL;
+                        $newcontent .= '* @Annotation\Attributes({"type":"checkbox"})' . PHP_EOL;
                         $newcontent .= '* @ORM\Column(type="boolean", nullable=' . $this->booleanString($property->getBeNullable()) . ', name="' . $property->getTblName() . '")/*' . PHP_EOL;
                         break;
                 }
@@ -79,18 +84,44 @@ class UpdateEntity {
             $file_contents = file_get_contents($path_to_file);
             $file_contents = preg_replace("/\/\/cdientitystart(.*\n)*\s*\/\/cdientityend/", $newcontent, $file_contents);
 
-            //   $file_contents = preg_replace("/\/\/cdientity/", $newcontent, $file_contents);
 
             file_put_contents($path_to_file, $file_contents);
+
+            if ($updateSchema) {
+                $cdientity_options = $this->getServiceLocator()->get('cdientity_options');
+
+                return exec($cdientity_options->getScriptUpdateSchema());
+            }
+            return null;
         }
     }
-    
-    protected function booleanString($value){
-        if($value){
+
+    protected function booleanString($value) {
+        if ($value) {
             return "true";
-        }else{
+        } else {
             return "false";
         }
+    }
+
+    /**
+     * Retrieve service manager instance
+     *
+     * @return ServiceManager
+     */
+    public function getServiceManager() {
+        return $this->serviceManager;
+    }
+
+    /**
+     * Set service manager instance
+     *
+     * @param ServiceManager $serviceManager
+     * @return User
+     */
+    public function setServiceManager(ServiceManager $serviceManager) {
+        $this->serviceManager = $serviceManager;
+        return $this;
     }
 
 }
